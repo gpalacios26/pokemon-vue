3b637beedcfc6a4e12872b9e6d30c91f1e073690
Gregory Palacios <gregpalacios26@gmail.com>
2021-11-04 18:11:20 -0500

    Primer commit vue pokemon game

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+
+  <div v-else>
+    <h1>Quién es este pokémon?</h1>
+
+    <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+
+    <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+
+    <div v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo Juego</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import PokemonPicture from '../components/PokemonPicture.vue';
+import PokemonOptions from '../components/PokemonOptions.vue';
+import getPokemonOptions from '../helpers/getPokemonOptions';
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: '',
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+    checkAnswer(selectedId) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correcto, ${this.pokemon.name}`;
+      } else {
+        this.message = `Oops, era ${this.pokemon.name}`;
+      }
+    },
+    newGame() {
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.mixPokemonArray();
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
